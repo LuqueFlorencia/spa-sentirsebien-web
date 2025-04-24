f26a60d870a95187d76aebf976d051af8e6603d8
fluque <florencia.luque@valkimia.com>
2025-04-23 21:47:46 -0300

    .

--- src/config/api.js
+++ src/config/api.js
@@ -3,6 +3,6 @@ dotenv.config();
 const API_BASE_URL = process.env.VITE_API_BASE_URL;
 console.log(API_BASE_URL);*/
 
-API_BASE_URL='https://spa-sentirsebien-api.onrender.com';
+const API_BASE_URL = 'https://spa-sentirsebien-api.onrender.com';
 
 export default API_BASE_URL;
