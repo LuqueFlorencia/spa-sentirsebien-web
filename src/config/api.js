6607078079299ec624b20e24c8b837cb45afbd2c
fluque <florencia.luque@valkimia.com>
2025-04-23 21:38:37 -0300

    Cambio url de conexion al server

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,8 @@
-const API_BASE_URL = 'https://spa-sentirsebien-api.onrender.com';
+/*import dotenv from 'dotenv';
+dotenv.config();
+const API_BASE_URL = process.env.VITE_API_BASE_URL;
+console.log(API_BASE_URL);*/
+
+API_BASE_URL='https://spa-sentirsebien-api.onrender.com';
 
 export default API_BASE_URL;
