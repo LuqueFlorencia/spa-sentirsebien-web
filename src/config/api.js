babd694648f9dd6ebba1f846d515e453564df8ff
fluque <florencia.luque@valkimia.com>
2025-05-23 11:56:54 -0300

    Fix url de back

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-//const API_BASE_URL = 'https://spa-sentirsebien-api.onrender.com';
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://spa-sentirsebien-api.onrender.com';
+//const API_BASE_URL = 'http://localhost:3000';
 
 export default API_BASE_URL;
